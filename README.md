d622b40c73d5295280b7afa1fced19db0cf6189f
DoubleMoon Dev <doublemmon.dev01@gmail.com>
2021-05-24 20:31:50 +0700

    Show chart dbm

---
+++ README.md
@@ -0,0 +1,1 @@
+# DexGuru DBM Chrome Extension
---
+++ script.js
@@ -0,0 +1,7 @@
+(async () => {
+  document.querySelector('.iframe-wrapper').innerHTML = `<iframe 
+    id="iframe"
+    src="https://dex.guru/token/0x0314e5a39806c30d67b869ee1bcdabee7e08de74-bsc" 
+    frameborder="0" />
+  `;
+})();
